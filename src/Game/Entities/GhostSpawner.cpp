13935554c519532fbd8a6ef0f7d9669a91c26978
Noel1827 <noel.vargas5@upr.edu>
2020-10-27 01:01:07 -0400

    another bug xD

--- src/Game/Entities/GhostSpawner.cpp
+++ src/Game/Entities/GhostSpawner.cpp
@@ -3,14 +3,14 @@
 GhostSpawner::GhostSpawner(int x, int y, int width, int height, ofImage spriteSheet, EntityManager* em): Entity(x, y, width, height){
     sprite = spriteSheet;
     this->em = em;
-    Ghost *PinkGhost = new Ghost(x, y, width, height, sprite, "pink");
-    em->entities.push_back(PinkGhost);
+    Ghost *RedGhost = new Ghost(x, y, width, height, sprite, "red");
+    em->entities.push_back(RedGhost);
     Ghost *OrangeGhost = new Ghost(x, y, width, height, sprite, "orange");
      em->entities.push_back(OrangeGhost);
     Ghost *BlueGhost = new Ghost(x, y, width, height, sprite, "blue");
      em->entities.push_back(BlueGhost);
-     Ghost *RedGhost = new Ghost(x,y, width, height, sprite, "red");
-     em->entities.push_back(RedGhost);
+     Ghost *PinkGhost = new Ghost(x,y, width, height, sprite, "pink");
+     em->entities.push_back(PinkGhost);
     
 }
 
