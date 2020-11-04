347feb895bca5822630da7d5d5aa484e59812c01
Ryan8702 <ryan.williams@gmail.com>
2020-11-03 17:32:19 -0800

    put the references

--- src/Game/States/GameOverState.cpp
+++ src/Game/States/GameOverState.cpp
@@ -2,7 +2,6 @@
 
 GameOverState::GameOverState()
 {
-    // cuanto toques el RE del restart no funcionará, tiene coordenadas de "start"
     restartButton = new Button(0, 0, ofGetWidth(), ofGetHeight(), "Click Anywhere To Start");
     youdied.load("images/you-died.png");
     // gameOver.load("images/gameOver.mp3");
@@ -32,6 +31,14 @@ void GameOverState::render()
     ofSetColor(255,255,255);
     ofDrawBitmapString("Score:" + to_string(score),ofGetWidth()-100,20);
     restartButton->render();
+    ofSetColor(255,170,0);
+    ofDrawBitmapString("Images were from:" , 0, ofGetHeight() / 1.3 + 100);
+    ofDrawBitmapString("https://c4.wallpaperflare.com/wallpaper/114/731/747/blue-pacman-gameboy-old-games-wallpaper-preview.jpg" , 0, ofGetHeight() / 1.3 + 110);
+    ofDrawBitmapString("https://i2.wp.com/nerdmacia.cl/wp-content/uploads/2016/02/you-died.png?fit=1920%2C934&ssl=1" , 0, ofGetHeight() / 1.3 + 120);
+    ofDrawBitmapString("Soundbytes were from:" , 0, ofGetHeight() / 1.3 + 130);
+    ofDrawBitmapString("https://www.classicgaming.cc/classics/pac-man/sounds" , 0, ofGetHeight() / 1.3 + 140);
+    ofDrawBitmapString("http://www.classicgaming.cc/classics/donkey-kong/sounds" , 0, ofGetHeight() / 1.3 + 150);
+    ofDrawBitmapString("https://www.101soundboards.com/sounds/11321-game-over" , 0, ofGetHeight() / 1.3 + 160);
 }
 
 void GameOverState::mousePressed(int x, int y, int button) {
