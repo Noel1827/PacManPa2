493e769602e930b1959abce5d43fae6cb548c3f8
Noel1827 <noel.vargas5@upr.edu>
2020-11-04 20:44:03 -0400

    Incomplete Ghosts move and flashing

--- src/Game/Entities/Ghost.cpp
+++ src/Game/Entities/Ghost.cpp
@@ -1,17 +1,129 @@
 #include "Ghost.h"
 
-Ghost::Ghost(int x, int y, int width, int height, ofImage spriteSheet, string color): Entity(x, y, width, height){
-
-    if (color == "pink")
-        sprite.cropFrom(spriteSheet, 456, 81, 16, 16);
-    else if (color == "blue")
-        sprite.cropFrom(spriteSheet, 456, 96, 16, 16);
-    else if (color == "orange")
-        sprite.cropFrom(spriteSheet, 456, 113, 16, 16);
-    else
-        sprite.cropFrom(spriteSheet, 456, 64, 16, 16);
-     
+Ghost::Ghost(int x, int y, int width, int height, ofImage spriteSheet, string color, EntityManager *em) : Entity(x, y, width, height)
+{
+    if (GhostFlash == false)
+    {
+        if (color == "pink")
+            sprite.cropFrom(spriteSheet, 456, 81, 16, 16);
+        else if (color == "blue")
+            sprite.cropFrom(spriteSheet, 456, 96, 16, 16);
+        else if (color == "orange")
+            sprite.cropFrom(spriteSheet, 456, 113, 16, 16);
+        else
+            sprite.cropFrom(spriteSheet, 456, 64, 16, 16);
+    }
+    else if (GhostFlash == true)
+    {
+        if (white == true)
+        {
+            sprite.cropFrom(spriteSheet, 617, 64, 16, 16);
+        }
+        else
+            sprite.cropFrom(spriteSheet, 584, 64, 16, 16);
+    }
 }
 
-// Dark blue sprite.cropFrom(spriteSheet, 584, 64, 16,16);
-// White ghosts sprite.cropFrom(spriteSheet, 617, 64, 16,16);
+void Ghost::checkCollisions()
+{
+    // for (Block *block : em->blocks)
+    // {
+    //     switch (Gfacing)
+    //     {
+    //     case GUP:
+    //         if (this->getBounds(x, y - speed).intersects(block->getBounds()))
+    //         {
+    //             canMove = false;
+    //         }
+    //         break;
+    //     case GDOWN:
+    //         if (this->getBounds(x, y + speed).intersects(block->getBounds()))
+    //         {
+    //             canMove = false;
+    //         }
+    //         break;
+    //     case GLEFT:
+    //         if (this->getBounds(x - speed, y).intersects(block->getBounds()))
+    //         {
+    //             canMove = false;
+    //         }
+    //         break;
+    //     case GRIGHT:
+    //         if (this->getBounds(x + speed, y).intersects(block->getBounds()))
+    //         {
+    //             canMove = false;
+    //         }
+    //         break;
+    //     }
+    // }
+    for (Entity *entity : em->entities)
+    {
+        if (collides(entity))
+        {
+            if (dynamic_cast<BigDot *>(entity))
+            {
+                GhostFlash  = true;
+            }
+        }
+    }
+}
+
+    void Ghost::tick()
+    {
+        if (GhostFlash == true)
+        {
+            flashing++;
+            counter++;
+            if (counter == 450)
+            {
+                GhostFlash = false;
+                counter = 0;
+            }
+            if(flashing == 30){
+                white = !white;
+                flashing = 0;
+            }
+         
+        }
+
+// -------------------------------------------------------------------------
+         canMove = true;
+        //  checkCollisions();
+        if (canMove)
+        {
+            if (Gfacing == GUP)
+            {
+                y -= speed;
+            }
+            else if (Gfacing == GDOWN)
+            {
+                y += speed;
+            }
+            else if (Gfacing == GLEFT)
+            {
+                x -= speed;
+
+            }
+            else if (Gfacing == GRIGHT)
+            {
+                x += speed;
+            }
+        }
+    }
+
+
+    // void Ghost::render(){
+    //     em->render();
+    // }
+
+    
+
+    // Dark blue sprite.cropFrom(spriteSheet, 584, 64, 16,16);
+    // White ghosts sprite.cropFrom(spriteSheet, 617, 64, 16,16);
+
+
+
+
+
+
+
